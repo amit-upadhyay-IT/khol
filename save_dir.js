06c6336e6fdd00767af9c2a969381467ad4a0b7f
amit-upadhyay-it <youarefreak1@gmail.com>
2018-02-07 01:53:38 +0530

    need changes in design

--- save_dir.js
+++ save_dir.js
@@ -40,7 +40,7 @@ function check_file(filename, short_name, dir_path)
         }
         else
         {
-            // continue doint opeation without any addition of dummy object
+            // continue doing opeation without any addition of dummy object
             save_data(filename, short_name, dir_path);
         }
     });
@@ -49,7 +49,7 @@ function check_file(filename, short_name, dir_path)
 function add_empty_object(filename, short_name, dir_path)
 {
     // adding an empty object in the file
-    fs.writeFile(filename, '{}', function (err) {
+    fs.writeFile(filename, '', function (err) {
         if (err)
             throw err;
         else
@@ -66,23 +66,38 @@ function save_data(write_filename, short_name, dir_path)
     // checking if the short_name entered is already present in the cache file or not
     // append into file only if short_name doesn't exists in the file
     console.log('short_name:', short_name);
-    check_short_name(short_name, write_filename).then(function(boolval) {
-        console.log('boolval:', boolval);
-        if (boolval)
+    check_short_name(short_name, write_filename).then(function(retval) {
+
+        console.log('retval:', retval);
+
+        // inserting a , before every insert
+        fs.appendFile(write_filename, JSON.stringify(data_obj), function (err) {
+            if (err)
+                throw err;
+            else
+                console.log('Success write');
+        });
+
+
+        /*
+        if (retval)
         {
             // i.e. short_name already exists
             console.log(short_name+' already exists, kindly chose some different');
         }
         else
         {
+            var o = retval;
+            o[short_name] = dir_path;
             // inserting a , before every insert
-            fs.appendFile(write_filename, ','+JSON.stringify(data_obj), function (err) {
+            fs.appendFile(write_filename, o, function (err) {
                 if (err)
                     throw err;
                 else
                     console.log('Success write');
             });
         }
+        */
     });
 }
 
@@ -100,15 +115,11 @@ function check_short_name(short_name, filename)
             }
             else
             {
-                console.log('check3');
-                // getting the json object from the data
-                // var content = JSON.parse(data);
-                var content = data;
                 // checing if short_name exists
-                console.log('content:', content);
-                console.log('content[short_name]:', content[short_name]);
-                console.log('content.short_name:', content.short_name);
-                if (content[short_name] == null)
+                var content = data;
+                resolve(data);
+                /*
+                if (data[short_name] == null)
                 {
                     console.log('check4');
                     resolve(false);
@@ -118,8 +129,9 @@ function check_short_name(short_name, filename)
                     console.log('check5');
                     resolve(true);
                 }
+                */
             }
-        })
+        });
 
     });
 
